3e2305934254ae35eccc8c6cf2c7092ab713d8b6
Jesse Thomsen <jesse.thomsen@mymail.champlain.edu>
2025-10-17 12:50:36 -0400

    Changed text

--- examples/catchthecat/Agent.cpp
+++ examples/catchthecat/Agent.cpp
@@ -53,7 +53,7 @@ std::vector<Point2D> Agent::generatePath(World* w) {
     }
 }
   // if the border is not infinity, build the path from border to the cat using the camefrom map
-  // if there isnt a reachable border, just return empty vector
+  // if there isn't a reachable border, just return empty vector
   // if your vector is filled from the border to the cat, the first element is the catcher move, and the last element is the cat move
 
 
